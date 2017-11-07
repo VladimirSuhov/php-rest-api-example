1384a8593b93b49ef9bbdcfd60811a02828b0556
sukhov-vl <vladimir.s.sukhov@gmail.com>
2017-11-07 17:16:42 +0300

    Сделал вывод продуктов

--- app/products/read-products.js
+++ app/products/read-products.js
@@ -1,4 +1,63 @@
 /**
  * Created by Vova on 07.11.2017.
  */
+$(document).ready(function () {
+
+    showProducts();
+    //
+    // $(document).on('click', '.read-products-button'), function () {
+    //     showProducts();
+    // }
+
+});
+
+function showProducts() {
+
+    let productsJSON;
+
+    $.getJSON("http://php-rest-api-example/api/product/read.php", function (data) {
+
+        let read_products_html =
+            `<div id='create-product' class='btn btn-primary pull-right m-b-15px create-product-button'>
+                <span class='glyphicon glyphicon-plus'></span> Create Product
+            </div>
+            <table class='table table-bordered table-hover'>
+                <tr>
+                    <th class='w-25-pct'>Name</th>
+                    <th class='w-10-pct'>Price</th>
+                    <th class='w-15-pct'>Category</th>
+                    <th class='w-25-pct text-align-center'>Action</th>
+                </tr>
+                ${renderRows(data)}
+            </table> `;
+
+        function renderRows(data) {
+            console.log(data.records);
+            return data.records.map((item, index) => {
+
+                return `<tr>
+                        <td>${item.name}</td>
+                        <td>${item.price}</td>
+                        <td>${item.category_name}</td>
+                        
+                        <td>
+                            <button class='btn btn-primary m-r-10px read-one-products-button' data-id='${item.id}'>
+                                <span class='glyphicon glyphicon-eye-open'></span> Read
+                            </button>
+                            
+                            <button class='btn btn-info m-r-10px update-product-button' data-id='${item.id}'>
+                                <span class='glyphicon glyphicon-eye-edit'></span> Edit
+                            </button>
+                            
+                            <button class='btn btn-danger m-r-10px delete-product-button' data-id='${item.id}'>
+                                <span class='glyphicon glyphicon-eye-remove'></span> Delete
+                            </button>
+                        </td>
+                    </tr>`;
+
+            })
+        }
+        $('#page-content').html(read_products_html);
+    });
+}
 
