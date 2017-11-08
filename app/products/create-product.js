f52acdc8a8443333e63b1e93d642703a91947dd9
sukhov-vl <vladimir.s.sukhov@gmail.com>
2017-11-08 15:18:08 +0300

    Переписал на es6

--- app/products/create-product.js
+++ app/products/create-product.js
@@ -1,75 +1,74 @@
 /**
  * Created by Vova on 07.11.2017.
  */
-$(document).ready(function () {
+
+class ProductForm {
+    construct(data) {
+        this.selectOptions = _renderCategoriesList(data);
+    }
+
+    _renderCategoriesList(data) {
+        return test.map(item =>{ return `<option value="${item.id}">${item.name}</option>` });
+    }
+
+    renderForm() {
+        return `<div id='read-products' class='btn btn-primary pull-right m-b-15px read-products-button'>
+            <span class='glyphicon glyphicon-list'></span> Read Products
+        </div>
+        <form id='create-product-form' action='#' method='post' border='0'>
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Name</td>
+                    <td><input type='text' name='name' class='form-control' required /></td>
+                </tr>
+                <tr>
+                    <td>Price</td>
+                    <td><input type='number' min='1' name='price' class='form-control' required /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control' required></textarea></td>
+                </tr>
+                <tr>
+                    <td>Category</td>
+                    <td>
+                        <select name='category_id' class='form-control'>
+                              ${this._renderCategoriesList()}
+                          </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <button type='submit' class='btn btn-primary'>
+                            <span class='glyphicon glyphicon-plus'></span> Create Product
+                        </button>
+                    </td>
+                </tr>
+            </table>
+        </form>`;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
 
 
-   $(document).on('click', '.create-product-button', createProduct) ;
-
-   function createProduct() {
-       fetch("http://php-rest-api-example/api/product/read.php").then(
-           function (response) {
-               if(response.status !== 200) {
-                   console.log('Looks like there was a problem. Status Code: ' + response.status);
-                   return;
-               }
-               
-               response.json().then(function (data) {
-
-                   let categories_options_html =
-                       `<select name='category_id' class='form-control'>
-                            ${renderCategotiesList(data)}
-                        </select> `;
-
-                   let create_product_html =
-                       `<div id='read-products' class='btn btn-primary pull-right m-b-15px read-products-button'>
-                            <span class='glyphicon glyphicon-list'></span> Read Products
-                        </div>
-                        <form id='create-product-form' action='#' method='post' border='0'>
-                            <table class='table table-hover table-responsive table-bordered'>
-                                <tr>
-                                    <td>Name</td>
-                                    <td><input type='text' name='name' class='form-control' required /></td>
-                                </tr>
-                                <tr>
-                                    <td>Price</td>
-                                    <td><input type='number' min='1' name='price' class='form-control' required /></td>
-                                </tr>
-                                <tr>
-                                    <td>Description</td>
-                                    <td><textarea name='description' class='form-control' required></textarea></td>
-                                </tr>
-                                <tr>
-                                    <td>Category</td>
-                                    <td>${categories_options_html}</td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td>
-                                        <button type='submit' class='btn btn-primary'>
-                                            <span class='glyphicon glyphicon-plus'></span> Create Product
-                                        </button>
-                                    </td>
-                                </tr>
-                            </table>
-                        </form>`;
-
-                   function renderCategotiesList(data) {
-                       return data.records.map((item, index)=> {
-                           return `<option value="${item.id}">${item.name}</option>`
-                       });
-                   }
-
-                   $("#page-content").html(create_product_html);
-               })
+    $(document).on('click', '.create-product-button', createProduct);
 
 
+    function createProduct(e) {
+        e.preventDefault();
 
-           // changePageTitle("Create Product");
-       })
-       .catch(function (err) {
-           console.log('Fetch Error :-S', err);
-       });
-   }
+        fetch("http://php-rest-api-example/api/product/read.php")
+            .then(response => response.json())
+                .then(data => {
+                    const form = new ProductForm(data.records);
+                    $('#page-content').html(form.renderForm());
+                })
+                .catch(function (err) {
+                    console.log('Fetch Error :-S', err);
+                });
 
+
+    };
 });
--- app/products/read-products.js
+++ app/products/read-products.js
@@ -1,73 +1,213 @@
 /**
  * Created by Vova on 07.11.2017.
  */
-$(document).ready(function () {
 
-    showProducts();
+var test = [
+    {
+        "id": "60",
+        "name": "Rolex Watch",
+        "description": "Luxury watch.",
+        "price": "25000",
+        "category_id": "1",
+        "category_name": "Fashion"
+    },
+    {
+        "id": "48",
+        "name": "Bristol Shoes",
+        "description": "Awesome shoes.",
+        "price": "999",
+        "category_id": "5",
+        "category_name": "Movies"
+    },
+    {
+        "id": "42",
+        "name": "Nike Shoes for Men",
+        "description": "Nike Shoes",
+        "price": "12999",
+        "category_id": "3",
+        "category_name": "Motors"
+    },
+    {
+        "id": "31",
+        "name": "Amanda Waller Shirt",
+        "description": "New awesome shirt!",
+        "price": "333",
+        "category_id": "1",
+        "category_name": "Fashion"
+    },
+    {
+        "id": "28",
+        "name": "Wallet",
+        "description": "You can absolutely use this one!",
+        "price": "799",
+        "category_id": "6",
+        "category_name": "Books"
+    },
+    {
+        "id": "26",
+        "name": "Another product",
+        "description": "Awesome product!",
+        "price": "555",
+        "category_id": "2",
+        "category_name": "Electronics"
+    },
+    {
+        "id": "13",
+        "name": "Abercrombie Allen Brook Shirt",
+        "description": "Cool red shirt!",
+        "price": "70",
+        "category_id": "1",
+        "category_name": "Fashion"
+    },
+    {
+        "id": "12",
+        "name": "Abercrombie Lake Arnold Shirt",
+        "description": "Perfect as gift!",
+        "price": "60",
+        "category_id": "1",
+        "category_name": "Fashion"
+    },
+    {
+        "id": "11",
+        "name": "Huawei Y300",
+        "description": "For testing purposes.",
+        "price": "100",
+        "category_id": "2",
+        "category_name": "Electronics"
+    },
+    {
+        "id": "10",
+        "name": "Sony Smart Watch",
+        "description": "The coolest smart watch!",
+        "price": "300",
+        "category_id": "2",
+        "category_name": "Electronics"
+    },
+    {
+        "id": "9",
+        "name": "Spalding Watch",
+        "description": "My sports watch.",
+        "price": "199",
+        "category_id": "1",
+        "category_name": "Fashion"
+    },
+    {
+        "id": "8",
+        "name": "Samsung Galaxy Tab 10.1",
+        "description": "Good tablet.",
+        "price": "259",
+        "category_id": "2",
+        "category_name": "Electronics"
+    },
+    {
+        "id": "7",
+        "name": "Lenovo Laptop",
+        "description": "My business partner.",
+        "price": "399",
+        "category_id": "2",
+        "category_name": "Electronics"
+    },
+    {
+        "id": "6",
+        "name": "Bench Shirt",
+        "description": "The best shirt!",
+        "price": "29",
+        "category_id": "1",
+        "category_name": "Fashion"
+    },
+    {
+        "id": "2",
+        "name": "Google Nexus 4",
+        "description": "The most awesome phone of 2013!",
+        "price": "299",
+        "category_id": "2",
+        "category_name": "Electronics"
+    },
+    {
+        "id": "1",
+        "name": "LG P880 4X HD",
+        "description": "My first awesome phone!",
+        "price": "336",
+        "category_id": "3",
+        "category_name": "Motors"
+    },
+    {
+        "id": "3",
+        "name": "Samsung Galaxy S4",
+        "description": "How about no?",
+        "price": "600",
+        "category_id": "3",
+        "category_name": "Motors"
+    }];
 
-    $(document).on('click', '.read-products-button', function () {
-        showProducts();
-    });
+class showProductsTable {
 
-});
+    construct() {
+        this.renderProductRows = _renderProductRows(data);
+    }
 
-function showProducts() {
+    _renderTableHeader() {
+        return `<div id='create-product' class='btn btn-primary pull-right m-b-15px create-product-button'>
+                    <span class='glyphicon glyphicon-plus'></span> Create Product
+                </div>
+                <table class='table table-bordered table-hover'>
+                    <tr>
+                        <th class='w-25-pct'>Name</th>
+                        <th class='w-10-pct'>Price</th>
+                        <th class='w-15-pct'>Category</th>
+                        <th class='w-25-pct text-align-center'>Action</th>
+                    </tr>
+                    ${this._renderProductRows()}
+                </table> `;
+    }
 
-    fetch("http://php-rest-api-example/api/product/read.php").then(
-        function (response) {
-            if(response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' + response.status);
-                return;
-            }
-
-            response.json().then(function (data) {
-
-        let read_products_html =
-            `<div id='create-product' class='btn btn-primary pull-right m-b-15px create-product-button'>
-                <span class='glyphicon glyphicon-plus'></span> Create Product
-            </div>
-            <table class='table table-bordered table-hover'>
-                <tr>
-                    <th class='w-25-pct'>Name</th>
-                    <th class='w-10-pct'>Price</th>
-                    <th class='w-15-pct'>Category</th>
-                    <th class='w-25-pct text-align-center'>Action</th>
-                </tr>
-                ${renderRows(data)}
-            </table> `;
-
-        function renderRows(data) {
-
-            return data.records.map((item, index) => {
-
-                return `<tr>
+    _renderProductRows(data) {
+        return test.map((item, index) => {
+            return `<tr>
                         <td>${item.name}</td>
                         <td>${item.price}</td>
                         <td>${item.category_name}</td>
-                        
+
                         <td>
                             <button class='btn btn-primary m-r-10px read-one-products-button' data-id='${item.id}'>
                                 <span class='glyphicon glyphicon-eye-open'></span> Read
                             </button>
-                            
+
                             <button class='btn btn-info m-r-10px update-product-button' data-id='${item.id}'>
                                 <span class='glyphicon glyphicon-eye-edit'></span> Edit
                             </button>
-                            
+
                             <button class='btn btn-danger m-r-10px delete-product-button' data-id='${item.id}'>
                                 <span class='glyphicon glyphicon-eye-remove'></span> Delete
                             </button>
                         </td>
                     </tr>`;
 
-            })
-        }
-        $('#page-content').html(read_products_html);
-    })
-    .catch(function (err) {
-        console.log('Fetch Error :-S', err);
-    });
-    // changePageTitle("Read Products");
-});
+        })
+    }
+
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    showProducts();
+
+    $(document).on('click', '.create-product-button', showProducts);
+
+    function showProducts() {
+
+        fetch("http://php-rest-api-example/api/product/read.php")
+            .then(response => response.json())
+            .then(data => {
+                const table = new showProductsTable(data.records);
+                $('#page-content').html(table._renderTableHeader());
+            })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            });
+
+
+    };
+});
+
 
